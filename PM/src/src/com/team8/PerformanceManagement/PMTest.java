38a8d88ad6a4b1a0dfa808a05833caed2caaf1b5
dyslove123 <7675343.die@163.com>
2015-04-24 09:59:32 +0800

    java doc
    Finish java doc for PM

--- PM/src/src/com/team8/PerformanceManagement/PMTest.java
+++ PM/src/src/com/team8/PerformanceManagement/PMTest.java
@@ -16,6 +16,9 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+
+import junit.framework.TestCase;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -23,33 +26,13 @@ import org.junit.Test;
 
 
 
-public class PMTest {
+public class PMTest extends TestCase{
 
-	//多线程测试类
-		public class testThread extends Thread
-		{
-			@Override
-			public void run()
-			{
-				int times=5;
-				while(times>0)
-				{
-					times--;
-					try {
-						sleep(30);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					
-					PM.sendPMMessage("TestThread", 1);
-				}
-				
-			}
-		}
+	
 	@Before
 	public void setUp() throws Exception {
 	}
+	//测试Reset()函数
 	@Test
 	public void testReset()
 	{
@@ -59,17 +42,16 @@ public class PMTest {
 		File file=new File(FileName);
 		PM.Reset();
 		assertFalse(file.exists());
+		assertTrue(PM.getMap().isEmpty());
 	}
-	
-	@Test
 	// 测试正常情况,0分钟延迟
+	@Test
 	public void testGetFileNameNormal() {
 		SimpleDateFormat target = new SimpleDateFormat("yyyy-MM-dd-HH-mm");// 设置日期格式
-		assertEquals(target.format(new Date())+".log", PM.getFileName(0));
+		assertEquals("LOG\\"+target.format(new Date())+".log", PM.getFileName(0));
 	}
-
-	@Test
 	// 测试时间超过一个小时的情况
+	@Test
 	public void testGetFileNameMore() {
 		PM.Reset();
 		SimpleDateFormat dft = new SimpleDateFormat("yyyy-MM-dd-HH-mm");
@@ -77,7 +59,7 @@ public class PMTest {
 		Calendar dar = Calendar.getInstance();
 		dar.setTime(date);
 		dar.add(Calendar.HOUR, 2);
-		String FileName = dft.format(dar.getTime())+".log";
+		String FileName = "LOG\\"+dft.format(dar.getTime())+".log";
 		String result = PM.getFileName(120);
 		assertEquals(FileName,result);
 	}
@@ -85,28 +67,39 @@ public class PMTest {
 	@Test
 	public void testSendPMMessage() {
 		PM.Reset();
-		String FileName=PM.getFileName(0);
 		PM.sendPMMessage("Alice", 12);
 		Map<String, Integer> tempMap=PM.getMap();
 		assertEquals(true,tempMap.containsKey("Alice"));
 		assertEquals(new Integer(12),tempMap.get("Alice"));
-		
-		PM.sendPMMessage("Alice", 10);
-		PM.sendPMMessage("Bob", 10);
-		tempMap=PM.getMap();
-		if (PM.getFileName(0).equals(FileName)) {
-			assertEquals(true,tempMap.containsKey("Alice"));
-			assertEquals(new Integer(22),tempMap.get("Alice"));
-		}
-		else{
-			assertEquals(true,tempMap.containsKey("Alice"));
-			assertEquals(new Integer(10),tempMap.get("Alice"));
-		}
+		PM.Reset();
 	}
-
 	@Test
-	//通过检查文件是否存在来测试void方法
-	//检测输出是否正确
+	public void testSendMorePMMessage() {
+		PM.Reset();
+		PM.sendPMMessage("Alice", 12);
+		PM.sendPMMessage("Bob", 10);
+		PM.sendPMMessage("Alice", 10);
+		Map<String, Integer> tempMap=PM.getMap();
+		assertEquals(true,tempMap.containsKey("Alice"));
+		assertEquals(new Integer(22),tempMap.get("Alice"));
+		assertEquals(true,tempMap.containsKey("Bob"));
+		assertEquals(new Integer(10),tempMap.get("Bob"));
+		PM.Reset();
+	}
+	@Test
+	public void testSendErrorPMMessage() {
+		PM.Reset();
+		PM.sendPMMessage("error", -1);
+		String FileName=PM.getFileName(0);
+		File file=new File(FileName);
+		assertFalse(file.exists());
+		
+		PM.sendPMMessage(null, -1);
+		assertFalse(file.exists());
+		
+	}
+	@Test
+	//通过读取log文件测试输出效果
 	public void testOutputNormal() {
 		PM.Reset();
 		PM.sendPMMessage("Loki", 18);
@@ -114,7 +107,7 @@ public class PMTest {
 		PM.sendPMMessage("Loki", 1000);
 		File target = new File(PM.getFileName(0));
 		assertTrue(target.exists());
-
+		
 		try {
 			BufferedReader bf=new BufferedReader(new FileReader(target));
 			String FirstLine=bf.readLine();
@@ -127,34 +120,65 @@ public class PMTest {
 			e.printStackTrace();
 		}			
 	}
-	//多线程测试
+	//多线程测试 压力测试1000个线程 每个线程写5次
 	@Test
-	public void testMutiThread()
+	public void testMutiThread() throws InterruptedException
 	{
 		PM.Reset();
-		Thread m1=new testThread();
-		Thread m2=new testThread();
-		Thread m3=new testThread();
-		Thread m4=new testThread();
-		m1.run();
-		m2.run();
-		m3.run();
-		m4.run();
+		int threadNum=1000;
+		CountDownLatch runningThreadNum = new CountDownLatch(threadNum);
+		for(int i=0;i<threadNum;i++)
+		{
+			new testThread(runningThreadNum).start();
+		}
+		runningThreadNum.await();
+		
 		File target = new File(PM.getFileName(0));
 		assertTrue(target.exists());
-		
 		try {
 			FileReader fr=new FileReader(target);
 			BufferedReader bf=new BufferedReader(fr);
 			String FirstLine=bf.readLine();
-			assertEquals(FirstLine, "TestThread:20");
+			assertEquals(FirstLine, "TestThread:"+threadNum*5);
 			bf.close();
 			fr.close();
+			PM.Reset();
+			assertFalse(target.exists());
+			assertTrue(PM.getMap().isEmpty());
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}	
-
+		
 	}
-
+	//多线程测试类
+	public class testThread extends Thread
+	{
+		CountDownLatch runingNum;
+		public testThread(CountDownLatch runingNum) {
+			// TODO Auto-generated constructor stub
+			this.runingNum=runingNum;
+		}
+		@Override
+		public void run()
+		{
+			int times=5;
+			while(times>0)
+			{
+				times--;
+				try {
+					sleep(300);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				PM.sendPMMessage("TestThread", 1);
+			}
+			runingNum.countDown();
+		}
+	}
+	public static void main(String[] args) {
+        junit.textui.TestRunner.run(
+            PMTest.class);
+	}
 }
